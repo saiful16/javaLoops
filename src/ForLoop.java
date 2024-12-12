5f5c1cb2b874df94fe63c2fc28d72e66a8cb1dc4
saiful16 <saifulislam.cse164.com>
2024-12-12 22:08:43 +0100

    for loop

---
+++ src/ForLoop.java
@@ -0,0 +1,9 @@
+public class ForLoop {
+//    print number  1 to 10
+public static void main(String[] args) {
+    int i = 1;
+    for (i = 1; i <= 10; i++) {
+        System.out.println(i);
+    }
+}
+}
