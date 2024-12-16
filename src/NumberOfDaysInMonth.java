a73398c5cf79210833d8ff4dc691a80f6fd48107
Saiful <saifulislam.cse164@gmail.com>
2024-12-16 21:07:53 +0100

    Enhanced switch statement

--- src/NumberOfDaysInMonth.java
+++ src/NumberOfDaysInMonth.java
@@ -26,7 +26,7 @@ public class NumberOfDaysInMonth {
         }
         else {
             switch (month){
-                case 1 :
+                case 1, 3, 5, 7, 8, 10, 12:
                     return 31;
                 case 2 :
                 {
@@ -36,26 +36,8 @@ public class NumberOfDaysInMonth {
                     else
                         return 28;
                 }
-                case 3 :
-                    return 31;
-                case 4 :
+                case 4, 6, 9, 11:
                     return 30;
-                case 5 :
-                    return 31;
-                case 6 :
-                    return 30;
-                case 7 :
-                    return 31;
-                case 8 :
-                    return 31;
-                case 9 :
-                    return 30;
-                case 10 :
-                    return 31;
-                case 11 :
-                    return 30;
-                case 12 :
-                    return 31;
             }
         }
 
