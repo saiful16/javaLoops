f210253d705ce54f95f09d9b00943a8f0b70f9fa
saiful16 <saifulislam.cse164.com>
2024-12-16 21:48:37 +0100

    factorial of a number

---
+++ src/Factorial.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void factorial(int n) {
+        int fact = 1;
+        if (n < 1) {
+            System.out.println("Invalid number");
+            return;
+        }
+        for (int i = n; i >=1; i--){
+            fact = fact * i;
+        }
+        System.out.println(fact);
+        return;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        factorial(n);
+    }
+}
