8d6dcf35a59341433b734f52331b0be8f4b3431d
saiful16 <saifulislam.cse164.com>
2024-12-16 21:49:52 +0100

    commit

--- src/Factorial.java
+++ src/Factorial.java
@@ -14,7 +14,7 @@ public class Factorial {
         return;
     }
 
-
+//main method
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
