bdf40c432025b451081707553b58966d85cade3b
Saiful <saifulislam.cse164@gmail.com>
2024-12-23 20:57:25 +0100

    last digit checker, factor printer

---
+++ src/FactorPrinter.java
@@ -0,0 +1,18 @@
+public class FactorPrinter {
+    public static void printFactors(int number){
+
+        if(number < 1){
+            System.out.println("Invalid Value");
+        } else {
+            for(int i = 1; i <= number; i++){
+                if(number % i == 0){
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        printFactors(10);
+    }
+}
---
+++ src/GreatestCommonDivisor.java
@@ -0,0 +1,11 @@
+public class GreatestCommonDivisor {
+    public static int getGreatestCommonDivisor(int first, int second) {
+        if (first < 10 || second < 10) {
+            return -1;
+        } else {
+
+        }
+
+        return -1;
+    }
+}
